7cee40cbca63febd142ecb84e42662e337dee6e1
mutugi-c <mutugichris@gmail.com>
2023-05-03 00:45:42 +0200

    Create function to populate an HTML list element for tasks

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ const taskList = [
   { description: 'Visit friends', completed: false },
 ]
 
+const toDoList = document.getElementById('to-do-list');
+
+function populateTaskList(arr) {
+  arr.forEach((task) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('to-do-item');
+    toDoItem.innerHTML = `
+    <input type='checkbox' class='check-button' />
+    <span class='task-description' contenteditable='true'>${task.description}</span>
+    <span class='fas fa-ellipsis-v'></span>
+    <span class='fas fa-trash trash-icon hide'></span>
+    `;
+    toDoList.appendChild(toDoItem);
+  });
+}
+
+populateTaskList(taskList);
