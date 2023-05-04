c77c7e3b3a17b05979c9f5cbad3868e3030948e4
mutugi-c <mutugichris@gmail.com>
2023-05-04 19:07:19 +0200

    Add option t save edited task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 import './styles.css';
-
-const taskList = [
-  { description: 'Go to the movies', completed: false },
-  { description: 'Play football', completed: false },
-  { description: 'Drink milk', completed: false },
-  { description: 'Visit friends', completed: false },
-];
+import ManageTasks from '../modules/manage_tasks';
 
 const toDoList = document.getElementById('to-do-list');
+const toDoForm = document.getElementById('to-do-form');
+const toDoInput = document.getElementById('to-do-input');
+
+const taskManager = new ManageTasks([]);
 
 function populateTaskList(arr) {
   arr.forEach((task) => {
     const toDoItem = document.createElement('li');
     toDoItem.classList.add('to-do-item');
     toDoItem.innerHTML = `
-    <input type='checkbox' class='check-button' />
-    <span class='task-description' contenteditable='true'>${task.description}</span>
-    <span class='fas fa-ellipsis-v'></span>
-    <span class='fas fa-trash trash-icon hide'></span>
+    <input type="checkbox" class="check-button" />
+    <span class="task-description" contenteditable="true">${task.description}</span>
+    <span class="fas fa-ellipsis-v"></span>
+    <span class="fas fa-trash trash-icon hide"></span>
     `;
     toDoList.appendChild(toDoItem);
+
+    const taskDescription = toDoItem.querySelector('.task-description');
+
+    // Save edited task on enter key press
+    taskDescription.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        const { index } = task; // object destructuring due to ESLint
+        const newDescription = taskDescription.textContent.trim().substring(2);
+        taskManager.editTask(index, newDescription);
+        // Remove focus from the edited element
+        taskDescription.blur();
+      }
+    });
+    
   });
 }
 
-populateTaskList(taskList);
+populateTaskList(taskManager.taskArr);
