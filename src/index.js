c1cb493adbc63e101326e33452c784e3a1dc43c0
mutugi-c <mutugichris@gmail.com>
2023-05-04 19:16:54 +0200

    Update the completed status of  a task

--- src/index.js
+++ src/index.js
@@ -79,4 +79,24 @@ toDoForm.addEventListener('submit', (event) => {
   }
 });
 
+// Update completed status
+const toggleCompleted = (toDoList, taskManager, index) => {
+  const toDoItem = toDoList.querySelector(`[data-index="${index}"]`);
+  if (!toDoItem) {
+    return;
+  }
+  const checkButton = toDoItem.querySelector('.check-button');
+  if (checkButton.checked) {
+    toDoItem.classList.add('completed');
+    taskManager.completedTask(index, true);
+    taskManager.storeTasksInLocalStorage();
+  } else {
+    toDoItem.classList.remove('completed');
+    taskManager.completedTask(index, false);
+    taskManager.storeTasksInLocalStorage();
+  }
+};
+
+toggleCompleted(toDoList, taskManager);
+
 populateTaskList(taskManager.taskArr);
