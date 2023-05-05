f124dc06cb3aae389451b5b451a04960228e0a9c
mutugi-c <mutugichris@gmail.com>
2023-05-05 12:16:47 +0200

    Create module to clear completed tasks

---
+++ src/modules/clear_completed.js
@@ -0,0 +1,13 @@
+const clearCompleted = (toDoList, manageTasks) => {
+    const toDoItems = toDoList.querySelectorAll('.to-do-item');
+    toDoItems.forEach((toDoItem) => {
+      const checkButton = toDoItem.querySelector('.check-button');
+      if (checkButton.checked) {
+        const index = toDoItem.dataset.index - 1;
+        manageTasks.removeTask(index);
+        toDoItem.remove();
+      }
+    });
+  };
+  
+  export {clearCompleted};
